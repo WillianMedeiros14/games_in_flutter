ca5db566a1c9f0ba6d3cc1eba40e317b11221df1
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-12-17 23:32:08 -0300

    feature: statusbar styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:games/features/myApp/presentation/pages/my_app.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
   runApp(
     const MyApp(),
   );
