480ad64659e6fc093bb01929b57c091b6cf4440b
Willian Medeiros <willianmedeiroslm@gmail.com>
2025-01-28 00:23:36 -0300

    feature: add animation_confetti

--- lib/features/ticTacToe/presentation/pages/tic_tac_toe_congratulations.dart
+++ lib/features/ticTacToe/presentation/pages/tic_tac_toe_congratulations.dart
@@ -28,127 +28,149 @@ class _TicTacToeCongratulationsState extends State<TicTacToeCongratulations> {
             fit: BoxFit.cover,
           ),
         ),
-        child: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                decoration: const BoxDecoration(
-                  color: Color(0x4D2631FF),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Color(0x4D2631FF),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
                   ),
-                ),
-                child: Column(
-                  spacing: 20,
-                  children: [
-                    Lottie.asset('assets/animations/animation_confetti.json'),
-                    Stack(
-                      children: [
-                        Container(
-                          height: 50,
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(0, 165, 78, 78),
-                            image: DecorationImage(
-                              image: AssetImage(
-                                  'assets/images/bg_bg_congratulations_phrase.png'),
-                              fit: BoxFit.contain,
-                            ),
+                  child: Column(
+                    spacing: 20,
+                    children: [
+                      Column(
+                        children: [
+                          Stack(
+                            children: [
+                              Lottie.asset(
+                                'assets/animations/animation_confetti.json',
+                              ),
+                              Column(
+                                spacing: 16,
+                                children: [
+                                  Stack(
+                                    children: [
+                                      Container(
+                                        height: 50,
+                                        decoration: const BoxDecoration(
+                                          color: Color.fromARGB(0, 165, 78, 78),
+                                          image: DecorationImage(
+                                            image: AssetImage(
+                                              'assets/images/bg_bg_congratulations_phrase.png',
+                                            ),
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ),
+                                      Center(
+                                        child: Container(
+                                          padding: const EdgeInsets.only(
+                                            top: 5,
+                                          ),
+                                          child: const Text(
+                                            "Parabéns",
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Center(
+                                    child: ClipRRect(
+                                      child: Image.asset(
+                                        'assets/images/trophy_congratulations.png',
+                                        height: 190,
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: const BoxDecoration(
+                          color: Color(0x802631FF),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
                           ),
                         ),
-                        Center(
-                          child: Container(
-                            padding: const EdgeInsets.only(top: 5),
-                            child: const Text(
-                              "Parabéns",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
+                        child: Center(
+                          child: Column(
+                            spacing: 16,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: Image.network(
+                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzHv_mPgImMZzJ_WhA-3xGRokBctCc6ia5iw&s",
+                                  width: 100,
+                                  height: 100,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
-                            ),
+                              const Text(
+                                "Willian Medeiros",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                    ClipRRect(
-                      child: Image.asset(
-                        'assets/images/trophy_congratulations.png',
-                        height: 190,
-                        fit: BoxFit.contain,
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: const BoxDecoration(
-                        color: Color(0x802631FF),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFECAC4C),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 32,
+                            vertical: 12,
+                          ),
+                        ),
+                        child: const Text(
+                          'Nova partida',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                      child: Center(
-                        child: Column(
-                          spacing: 16,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(100),
-                              child: Image.network(
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzHv_mPgImMZzJ_WhA-3xGRokBctCc6ia5iw&s",
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const Text(
-                              "Willian Medeiros",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 32,
+                            vertical: 12,
+                          ),
+                        ),
+                        child: const Text(
+                          'Recomeçar jogo',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFFECAC4C),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 32,
-                          vertical: 12,
-                        ),
-                      ),
-                      child: const Text(
-                        'Nova partida',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 32,
-                          vertical: 12,
-                        ),
-                      ),
-                      child: const Text(
-                        'Recomeçar jogo',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
